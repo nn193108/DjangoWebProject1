dbdccbfc87edd599daf530677a456247d5a19f1e
RedCross321 <gagileveu@mail.ru>
2025-05-20 16:38:44 +0500

    First Commit

--- app/models.py
+++ app/models.py
@@ -3,16 +3,21 @@ Definition of models.
 """
 
 from django.db import models
-from django.utils import timezone
-from django.urls import reverse
 
 # Create your models here.
+from datetime  import datetime 
+from django.urls import reverse
+from django.utils import timezone
+from django.contrib import admin
+from django.contrib.auth.models import User
 
 class Blog(models.Model):
-    title = models.CharField(max_length=200, verbose_name="Заголовок")
-    summary = models.TextField(verbose_name="Краткое содержание")
+    title = models.CharField(max_length=100, unique_for_date="posted", verbose_name="Заголовок")
+    description = models.TextField(verbose_name="Краткое содержание")
     content = models.TextField(verbose_name="Полное содержание")
-    pub_date = models.DateTimeField(default=timezone.now, verbose_name="Дата публикации")
+    posted = models.DateTimeField(db_index=True, auto_now_add=True, verbose_name="Опубликована")
+    author = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, verbose_name="Автор")
+    image = models.FileField(upload_to='blog/', default='temp.jpg', verbose_name="Путь к картинке")
 
     def __str__(self):
         return self.title
@@ -21,6 +26,25 @@ class Blog(models.Model):
         return reverse('blog_detail', args=[str(self.id)])
 
     class Meta:
+        db_table = "Posts"
+        ordering = ["-posted"]
         verbose_name = "статья блога"
         verbose_name_plural = "статьи блога"
-        ordering = ['-pub_date']
+
+class Comment(models.Model):
+    text = models.TextField(verbose_name="Комментарий")
+    date = models.DateTimeField(default=timezone.now, verbose_name="Дата")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор")
+    post = models.ForeignKey(Blog, on_delete=models.CASCADE, verbose_name="Статья")
+
+    def __str__(self):
+        return f"{self.author} - {self.date}"
+
+    class Meta:
+        db_table = "Comments"
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии к статьям блога"
+        ordering = ['-date']
+
+admin.site.register(Blog)
+admin.site.register(Comment)
--- app/tests.py
+++ app/tests.py
@@ -6,13 +6,12 @@ when you run "manage.py test".
 import django
 from django.test import TestCase
 
-# TODO: Configure your database in settings.py and sync before running tests.
 
 class ViewTest(TestCase):
     """Tests for the application views."""
 
     if django.VERSION[:2] >= (1, 7):
-        # Django 1.7 requires an explicit setup() when running tests in PTVS
+       
         @classmethod
         def setUpClass(cls):
             super(ViewTest, cls).setUpClass()
